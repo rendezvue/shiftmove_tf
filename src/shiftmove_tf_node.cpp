c20b17ab9cccd053b1fa6ecbeeca2d4710220842
hjpark <hjpark@rdv.ai>
2023-05-18 16:41:12 +0900

    pkg name changed

--- src/shiftmove_tf_node.cpp
+++ src/shiftmove_tf_node.cpp
@@ -13,7 +13,7 @@
 
 tf2_ros::Buffer tfBuffer;
 
-geometry_msgs::Transform send_tfpose_navi(float x, float y, std::string str_tfpose_navi)
+geometry_msgs::Transform send_shiftmove_tf(float x, float y, std::string str_shiftmove_tf)
 {
     while(ros::ok()){
         static tf::TransformBroadcaster odom_broadcaster;        
@@ -24,11 +24,11 @@ geometry_msgs::Transform send_tfpose_navi(float x, float y, std::string str_tfpo
         tf2::Quaternion tf2_q;
         tf2_q.setRPY(0,0,0);
         new_tf.setRotation( tf::Quaternion( tf2_q.x(), tf2_q.y(), tf2_q.z(), tf2_q.w()) );
-        odom_broadcaster.sendTransform(tf::StampedTransform(new_tf, ros::Time::now(), "base_footprint", str_tfpose_navi));
+        odom_broadcaster.sendTransform(tf::StampedTransform(new_tf, ros::Time::now(), "base_footprint", str_shiftmove_tf));
 
         geometry_msgs::TransformStamped transform;
         try{
-            transform = tfBuffer.lookupTransform("map", str_tfpose_navi, ros::Time(0));
+            transform = tfBuffer.lookupTransform("map", str_shiftmove_tf, ros::Time(0));
         }
         catch (tf2::TransformException &ex){
             ROS_ERROR("%s", ex.what());
@@ -42,7 +42,7 @@ geometry_msgs::Transform send_tfpose_navi(float x, float y, std::string str_tfpo
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "tfpose_navi");
+    ros::init(argc, argv, "shiftmove_tf");
     ros::NodeHandle node;
     ros::NodeHandle nhp("~");
 
@@ -57,22 +57,22 @@ int main(int argc, char** argv)
 
     // fprintf(stderr,"param_front_dist = %f\n",param_front_dist);
 
-    ros::Publisher tfpose_navi_front = node.advertise<geometry_msgs::Transform>("tfpose_navi/front", 10);
-    ros::Publisher tfpose_navi_back = node.advertise<geometry_msgs::Transform>("tfpose_navi/back", 10);
-    ros::Publisher tfpose_navi_left = node.advertise<geometry_msgs::Transform>("tfpose_navi/left", 10);
-    ros::Publisher tfpose_navi_right = node.advertise<geometry_msgs::Transform>("tfpose_navi/right", 10);
+    ros::Publisher shiftmove_tf_front = node.advertise<geometry_msgs::Transform>("shiftmove_tf/front", 10);
+    ros::Publisher shiftmove_tf_back = node.advertise<geometry_msgs::Transform>("shiftmove_tf/back", 10);
+    ros::Publisher shiftmove_tf_left = node.advertise<geometry_msgs::Transform>("shiftmove_tf/left", 10);
+    ros::Publisher shiftmove_tf_right = node.advertise<geometry_msgs::Transform>("shiftmove_tf/right", 10);
 
     ros::Rate r(10);
 
     while(ros::ok()){
-        // tfpose_navi_front.publish(send_tfpose_navi(2,0,"tfpose_front"));
-        // tfpose_navi_back.publish(send_tfpose_navi(-2,0,"tfpose_back"));
-        // tfpose_navi_left.publish(send_tfpose_navi(0,2,"tfpose_left"));
-        // tfpose_navi_right.publish(send_tfpose_navi(0,-2,"tfpose_right"));
-        tfpose_navi_front.publish(send_tfpose_navi(param_front_dist,0,"tfpose_front"));
-        tfpose_navi_back.publish(send_tfpose_navi(param_back_dist,0,"tfpose_back"));
-        tfpose_navi_left.publish(send_tfpose_navi(0,param_left_dist,"tfpose_left"));
-        tfpose_navi_right.publish(send_tfpose_navi(0,param_right_dist,"tfpose_right"));
+        // shiftmove_tf_front.publish(send_shiftmove_tf(2,0,"tfpose_front"));
+        // shiftmove_tf_back.publish(send_shiftmove_tf(-2,0,"tfpose_back"));
+        // shiftmove_tf_left.publish(send_shiftmove_tf(0,2,"tfpose_left"));
+        // shiftmove_tf_right.publish(send_shiftmove_tf(0,-2,"tfpose_right"));
+        shiftmove_tf_front.publish(send_shiftmove_tf(param_front_dist,0,"tfpose_front"));
+        shiftmove_tf_back.publish(send_shiftmove_tf(param_back_dist,0,"tfpose_back"));
+        shiftmove_tf_left.publish(send_shiftmove_tf(0,param_left_dist,"tfpose_left"));
+        shiftmove_tf_right.publish(send_shiftmove_tf(0,param_right_dist,"tfpose_right"));
         r.sleep();
     }
     ros::spin();
